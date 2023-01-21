3e9970a579a78b19505788e97d5769d4898ae552
Chebel405 <anne.chebel@gmail.com>
2023-01-21 19:29:40 +0100

    Installation du projet php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="public/css/style.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+  <script async src="public/js/index.js"></script>
+  <title>Todo</title>
+</head>
+
+<body>
+  <div class="container">
+    <header>
+      <div class="logo">Dyma Todo</div>
+    </header>
+    <div class="content">
+      <div class="todo-container">
+        <h1>Ma Todo</h1>
+        <div class="todo-form"></div>
+        <div class="todo-list"></div>
+      </div>
+    </div>
+    <footer>2011 / 2021 © Tous droits réservés</footer>
+  </div>
+</body>
+
+</html>
